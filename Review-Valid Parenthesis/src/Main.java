af3539223c95a232a314a111cc10064495dc391a
Marshad <marshadahamedh@gmail.com>
2026-07-05 21:30:35 +0530

    review: week 1 day 4 solved

---
+++ Review-Valid Parenthesis/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isValidParentheses = IsValidParentheses.isValidParentheses("([)]");
+        System.out.println(isValidParentheses);
+    }
+}
