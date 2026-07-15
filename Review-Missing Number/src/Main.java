c216ef85b878d307453c9635603f8444d615fad9
Marshad <marshadahamedh@gmail.com>
2026-07-15 20:53:10 +0530

    review: week 2 day 7

---
+++ Review-Missing Number/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums= {};
+        int missingNum = ReturnMissingNumber.missingNumber(nums);
+        System.out.println(missingNum);
+    }
+}
---
+++ Review-Missing Number/src/ReturnMissingNumber.java
@@ -0,0 +1,15 @@
+public class ReturnMissingNumber {
+    public static int missingNumber(int[] nums) {
+
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int num : nums) {
+            sum1 += num;
+        }
+        int length  = nums.length;
+        for (int i = 0; i <= length; i++) {
+            sum2 += i;
+        }
+        return sum2 - sum1;
+    }
+}
