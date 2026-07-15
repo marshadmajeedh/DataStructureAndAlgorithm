7456d80c8ca4fda1839d4b8badd39874afdbe26f
Marshad <marshadahamedh@gmail.com>
2026-07-15 20:42:54 +0530

    review: week 2 day 5

---
+++ Review-Isomorphic Strings/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "abe";
+        String t = "agb";
+        boolean isIsomorphic = AreTwoStringIsomorphic.isIsomorphic(s,t);
+        System.out.println(isIsomorphic);
+    }
+}
